e9f23638ed78dd76657c13d48c2cf2144119bfd3
lionelsellam <lionelsellam@hotmail.com>
2023-01-20 02:07:58 +0200

    begining

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  // State
+
+  // Behavior
+  const handleSubmit = () => {
+
+  }
+
+  // Render
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bienvenue chez nous !</h1>
+      <h2>Connectez-vous</h2>
+      <form>
+        <label>
+          <button placeholder="Entrez votre prenom" onSubmit={handleSubmit}>
+            Accedez a votre espace
+          </button>
+        </label>
+      </form>
     </div>
   );
 }
