6e3b2b551ae034835e5484ed4d6d77db4c0efb57
lionelsellam <lionelsellam@hotmail.com>
2023-01-23 17:28:33 +0200

    ✨ F01: LOGIN > Create LoginPage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import "./App.css";
+import LoginPage from "./components/Pages/LoginPage";
 
 const App = () => {
   // State
 
   // Behavior
-  const handleSubmit = () => {};
 
   // Render
 
   return (
-    <div className="App">
-      <h1>Bienvenue chez nous !</h1>
-      <h2>Connectez-vous</h2>
-      <form>
-        <label>
-          <button placeholder="Entrez votre prenom" onSubmit={handleSubmit}>
-            Accedez a votre espace
-          </button>
-        </label>
-      </form>
-    </div>
+    <LoginPage/>
   );
 };
 
